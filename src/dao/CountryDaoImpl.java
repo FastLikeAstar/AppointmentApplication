35ab9990872f23b7ce9d73a08a024dcedeb4dadb
Kyle Guillory <fastlikeastar@gmail.com>
2023-01-11 13:02:39 -0700

    Country DAO Began

--- src/dao/CountryDaoImpl.java
+++ src/dao/CountryDaoImpl.java
@@ -1,125 +1,34 @@
 package dao;
 
+import javafx.collections.ObservableList;
+import sample.Country;
+
 import java.time.ZonedDateTime;
 
 public class CountryDaoImpl implements CountryDao{
-    @Override
-    public void createCountryId(int newId) {
 
+    @Override
+    public ObservableList<Country> getAllCountries() {
+        return null;
     }
 
     @Override
-    public void createCountryName(String newName) {
-
+    public Country getById(int id) {
+        return null;
     }
 
     @Override
-    public void createCreatedDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void createCreatedBy(String newName) {
-
-    }
-
-    @Override
-    public void createLastUpdateDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void createLastUpdatedBy(String newName) {
-
-    }
-
-    @Override
-    public int getCountryId() {
+    public int save(Country country) {
         return 0;
     }
 
     @Override
-    public String getCountryName() {
-        return null;
+    public int update(Country country) {
+        return 0;
     }
 
     @Override
-    public ZonedDateTime getCreatedDate() {
-        return null;
-    }
-
-    @Override
-    public String getCreatedBy() {
-        return null;
-    }
-
-    @Override
-    public ZonedDateTime getLastUpdateDate() {
-        return null;
-    }
-
-    @Override
-    public String getLastUpdatedBy() {
-        return null;
-    }
-
-    @Override
-    public void updateCountryId(int newId) {
-
-    }
-
-    @Override
-    public void updateCountryName(String newName) {
-
-    }
-
-    @Override
-    public void updateCreatedDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void updateCreatedBy(String newName) {
-
-    }
-
-    @Override
-    public void updateLastUpdateDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void updateLastUpdatedBy(String newName) {
-
-    }
-
-    @Override
-    public void deleteCountryId() {
-
-    }
-
-    @Override
-    public void deleteCountryName() {
-
-    }
-
-    @Override
-    public void deleteCreatedDate() {
-
-    }
-
-    @Override
-    public void deleteCreatedBy() {
-
-    }
-
-    @Override
-    public void deleteLastUpdateDate() {
-
-    }
-
-    @Override
-    public void deleteLastUpdatedBy() {
+    public void delete(int id) {
 
     }
 }
