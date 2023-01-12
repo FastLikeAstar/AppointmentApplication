728fee2a7243981658f094f70883ae8df88cbb4a
Kyle Guillory <fastlikeastar@gmail.com>
2023-01-12 14:00:01 -0700

    Added User Dao. Concluded DAO implementation.

--- src/dao/UserDaoImpl.java
+++ src/dao/UserDaoImpl.java
@@ -1,145 +1,154 @@
 package dao;
 
-import java.time.ZonedDateTime;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import sample.User;
+import sample.Jdbc;
+import java.sql.*;
+
 
 public class UserDaoImpl implements UserDao{
-    @Override
-    public void createUserId(int newId) {
 
+    @Override
+    public ObservableList<User> getAllUsers() {
+        ObservableList<User> userList = FXCollections.observableArrayList();
+        String sql = "SELECT * FROM users";
+        try {
+            Connection connection = Jdbc.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet results = statement.executeQuery();
+
+            while(results.next()){
+                int userId = results.getInt("User_ID");
+                String userName = results.getString("User_Name");
+                String password = results.getString("Password");
+                Timestamp createdDate = results.getTimestamp("Create_Date");
+                String createdBy = results.getString("Created_By");
+                Timestamp lastUpdate = results.getTimestamp("Last_Update");
+                String lastUpdatedBy = results.getString("Last_Updated_By");
+
+                User user = new User(userId, userName, password, createdDate, createdBy, lastUpdate, lastUpdatedBy);
+                userList.add(user);
+            }
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return userList;
     }
 
     @Override
-    public void createUserName(String newName) {
+    public User getById(int id) {
+        User userIfExists = null;
+        String sql = "SELECT * FROM users WHERE User_ID = ?";
 
+        try {
+            Connection connection = Jdbc.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, id);
+            ResultSet results = statement.executeQuery();
+
+            int userId = results.getInt("User_ID");
+            String userName = results.getString("User_Name");
+            String password = results.getString("Password");
+            Timestamp createdDate = results.getTimestamp("Create_Date");
+            String createdBy = results.getString("Created_By");
+            Timestamp lastUpdate = results.getTimestamp("Last_Update");
+            String lastUpdatedBy = results.getString("Last_Updated_By");
+
+            userIfExists = new User(userId, userName, password, createdDate, createdBy, lastUpdate, lastUpdatedBy);
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return userIfExists;
     }
 
     @Override
-    public void createPassword(String newName) {
+    public int save(User user) {
 
+
+        int affectedRows = -1;
+        String sql = "INSERT INTO users "+
+                "(User_Name, Password, Create_Date, Created_By, Last_Update, Last_Updated_By)"+
+                " VALUES (?, ?, NOW(), ?, NOW(), ?)";
+
+        try{
+            Connection connection = Jdbc.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            statement.setString(1,user.getUserName());
+            statement.setString(2,user.getPassword());
+            statement.setString(3,user.getCreatedBy());
+            statement.setString(4,user.getLastUpdatedBy());
+
+            affectedRows = statement.executeUpdate();
+            ResultSet generatedKey = statement.getGeneratedKeys();
+            user.setUserId(generatedKey.getInt(1));
+
+            // If SQL statement fails or affectedRows included in case the database is full (should see and Int rollover).
+            if (affectedRows <= 0){
+                throw new SQLException("Could not create user. -1 or 0 denotes no rows affected."
+                        + "Rows affected:" + affectedRows);
+            }
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return affectedRows;
     }
 
     @Override
-    public void createCreatedDate(ZonedDateTime newDate) {
+    public int update(User user) {
+        int affectedRows = -1;
+        String sql = "UPDATE users " +
+                "SET User_Name = ?, " +
+                "SET Password = ?, " +
+                "SET Last_Update = NOW(), " +
+                "SET Last_Updated_By = ?, " +
+                "WHERE User_ID = ?";
 
+        try{
+            Connection connection = Jdbc.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setString(1,user.getUserName());
+            statement.setString(2,user.getPassword());
+            statement.setString(3,user.getLastUpdatedBy());
+            statement.setInt(4,user.getUserId());
+
+            affectedRows = statement.executeUpdate();
+
+            // If SQL statement fails or affectedRows included in case the database is full (should see and Int rollover).
+            if (affectedRows <= 0){
+                throw new SQLException("Could not update user. -1 or 0 denotes no rows affected."
+                        + "Rows affected:" + affectedRows);
+            }
+
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
+
+        return affectedRows;
     }
 
     @Override
-    public void createCreatedBy(String newName) {
+    public void delete(int id) {
+        String sql = "DELETE FROM users WHERE User_ID = ?";
 
-    }
+        try{
+            Connection connection = Jdbc.getConnection();
+            PreparedStatement statement = connection.prepareStatement(sql);
 
-    @Override
-    public void createLastUpdateDate(ZonedDateTime newDate) {
+            statement.setInt(1,id);
 
-    }
+            statement.executeUpdate();
 
-    @Override
-    public void createLastUpdatedBy(String newName) {
-
-    }
-
-    @Override
-    public int getUserId() {
-        return 0;
-    }
-
-    @Override
-    public String getUserName() {
-        return null;
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public ZonedDateTime getCreatedDate() {
-        return null;
-    }
-
-    @Override
-    public String getCreatedBy() {
-        return null;
-    }
-
-    @Override
-    public ZonedDateTime getLastUpdateDate() {
-        return null;
-    }
-
-    @Override
-    public String getLastUpdatedBy() {
-        return null;
-    }
-
-    @Override
-    public void updateUserId(int newId) {
-
-    }
-
-    @Override
-    public void updateUserName(String newName) {
-
-    }
-
-    @Override
-    public void updatePassword(String newPassword) {
-
-    }
-
-    @Override
-    public void updateCreatedDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void updateCreatedBy(String newName) {
-
-    }
-
-    @Override
-    public void updateLastUpdateDate(ZonedDateTime newDate) {
-
-    }
-
-    @Override
-    public void updateLastUpdatedBy(String newName) {
-
-    }
-
-    @Override
-    public void deleteUserId() {
-
-    }
-
-    @Override
-    public void deleteUserName() {
-
-    }
-
-    @Override
-    public void deletePassword() {
-
-    }
-
-    @Override
-    public void deleteCreatedDate() {
-
-    }
-
-    @Override
-    public void deleteCreatedBy() {
-
-    }
-
-    @Override
-    public void deleteLastUpdateDate() {
-
-    }
-
-    @Override
-    public void deleteLastUpdatedBy() {
-
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
