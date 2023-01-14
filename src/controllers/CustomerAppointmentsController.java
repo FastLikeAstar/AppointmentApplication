6320f137718b28c0e2e6b63e54c90c2080a6f0b3
Kyle Guillory <fastlikeastar@gmail.com>
2023-01-13 22:31:33 -0700

    Began appointment work.

--- src/controllers/CustomerAppointmentsController.java
+++ src/controllers/CustomerAppointmentsController.java
@@ -1,12 +1,16 @@
 package controllers;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import sample.Customer;
 import sample.Main;
 
 import java.io.IOException;
@@ -14,6 +18,29 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CustomerAppointmentsController implements Initializable {
+    public Button buttonSaveChanges;
+    public Button buttonCancelChanges;
+    public Button buttonBack;
+    public Button buttonNewAppointment;
+    public Button buttonEditAppointment;
+    public Button buttonCancelAppointment;
+    public Label labelFeedback;
+    public Tab tabWeek;
+    public TableView tableWeekView;
+    public Tab tabMonth;
+    public TableView tableMonthView;
+    public Label labelSelectedAppointment;
+    public TextField textFieldAppointmentId;
+    public TextField textFieldTitle;
+    public TextField textFieldDescription;
+    public ComboBox comboCustomer;
+    public ComboBox comboType;
+    public ComboBox comboUser;
+    public ComboBox comboContact;
+    public TextField textFieldStartTime;
+    public TextField textFieldEndTime;
+    public TextField textFieldLocation;
+
     /**
      * @param url
      * @param resourceBundle
@@ -21,6 +48,33 @@ public class CustomerAppointmentsController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+    }
+
+    public void LoadTable(){
+        tableCustomerRecords.getItems().clear();
+        tableCustomerRecords.getColumns().clear();
+        ObservableList<Customer> customers = Main.dbCustomers.getAllCustomers();
+
+        TableColumn<Customer, Integer> columnCustomerId = new TableColumn<>("ID");
+        TableColumn<Customer, String> columnCustomerName = new TableColumn<>("Name");
+        TableColumn<Customer, String> columnPhoneNumber = new TableColumn<>("Phone");
+        TableColumn<Customer, String> columnAddress = new TableColumn<>("Address");
+        TableColumn<Customer, String> columnPostalCode = new TableColumn<>("Postal Code");
+        TableColumn<Customer, String> columnFirstDiv = new TableColumn<>("Division");
+        TableColumn<Customer, String> columnCountry = new TableColumn<>("Country");
+
+        columnCustomerId.setCellValueFactory(new PropertyValueFactory<>("customerId"));
+        columnCustomerName.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        columnPhoneNumber.setCellValueFactory(new PropertyValueFactory<>("phone"));
+        columnAddress.setCellValueFactory(new PropertyValueFactory<>("address"));
+        columnPostalCode.setCellValueFactory(new PropertyValueFactory<>("postalCode"));
+        columnFirstDiv.setCellValueFactory(new PropertyValueFactory<>("division"));
+        columnCountry.setCellValueFactory(new PropertyValueFactory<>("country"));
+
+
+        tableCustomerRecords.getColumns().addAll(columnCustomerId, columnCustomerName, columnPhoneNumber, columnAddress, columnPostalCode, columnFirstDiv, columnCountry);
+
+        tableCustomerRecords.setItems(customers);
     }
 
     public void CreateNewAppointment(ActionEvent actionEvent) {
